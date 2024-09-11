5a0f89c4f3fddd7bd3e627c1db7381eea91e7f94
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-09-11 16:30:53 +0700

    vuejs 11/9

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import HomeView from '@/views/HomeView.vue'
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+
+
+    }
+  ]
+})
+
+export default router
