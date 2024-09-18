9c86a2998bdb5d707f0438e43bfd936df6b94cf7
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-09-18 15:22:12 +0700

    xogn bai 6

---
+++ src/views/BlogView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="wrapper">
+    <!-- Header Container
+================================================== -->
+
+    <!-- Titlebar
+================================================== -->
+    <the-title-bar></the-title-bar>
+    <!-- Content
+================================================== -->
+    <the-content-blog></the-content-blog>
+
+    <!-- Back To Top Button -->
+    <div id="backtotop"><a href="#"></a></div>
+  </div>
+</template>
+
+<script>
+import TheTitleBar from "@/components/TheTitleBar.vue";
+import TheContentBlog from "@/components/TheContentBlog.vue";
+export default {
+  components: { TheTitleBar, TheContentBlog },
+};
+</script>
+
+<style></style>
